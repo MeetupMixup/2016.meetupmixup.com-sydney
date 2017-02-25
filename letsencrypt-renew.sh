e12eb4f87d3fb0426f0c06ffd5eda3cc78780aa9
Jack Skinner '; -- <jack@developerjack.com>
2017-02-25 20:10:14 +1100

    Removing certbot-auto from renewal script

--- letsencrypt-renew.sh
+++ letsencrypt-renew.sh
@@ -6,9 +6,7 @@ EXPIRE_THRESHOLD=$((86400 * 30)) #30 days
 
 
 function lebot() {
-    if hash ./certbot-auto 2>/dev/null; then
-        ./certbot-auto "$@"
-    elif hash letsencrypt 2>/dev/null; then
+    if hash letsencrypt 2>/dev/null; then
         letsencrypt "$@"
     elif hash certbot 2>/dev/null; then
         certbot "$@"
